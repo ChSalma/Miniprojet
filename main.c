35de1c45ad6c616eaeb8ebce833232deee6df280
Salma Chatagny <salma.chatagny@epfl.ch>
2020-04-06 13:27:11 +0200

    Proximity.c accessible
    quelques petits tests en commentaires qui fonctionnent mais je n'ai pas encore la conversion dist/prox correcte

--- main.c
+++ main.c
@@ -13,12 +13,27 @@
 #include <process_sensor.h>
 #include <chprintf.h>
 
-#define IR0 0
+//define pour la conversion en mm
+#define DIV_FACTOR_DIST 16
+#define OFFSET_DIST 54
 
 //Je suis pas sûre de ces lignes, copier du main de chibi--> m'a permis d'éviter l'erreur de thd dans proximity.c
+//Déclaration du bus
 messagebus_t bus;
 MUTEX_DECL(bus_lock);
 CONDVAR_DECL(bus_condvar);
+
+static void serial_start(void)
+{
+	static SerialConfig ser_cfg = {
+	    115200,
+	    0,
+	    0,
+	    0,
+	};
+
+	sdStart(&SD3, &ser_cfg); // UART3.
+}
 
 int main(void)
 {
@@ -27,9 +42,14 @@ int main(void)
     chSysInit();
     mpu_init();
 
+    //Init du bus
+    messagebus_init(&bus, &bus_lock, &bus_condvar);
+    //starts the serial communication
+    serial_start();
+
     //Init des capteurs de proximité
-//	  proximity_start();
-//    calibrate_ir();
+    proximity_start();
+    calibrate_ir();
 
 	//Init les moteurs
 	motors_init();
@@ -42,15 +62,27 @@ int main(void)
 
 	//init de test values
 	int test_value[2];
+	int i;
 
     /* Infinite loop. */
     while (1) {
-    	//test des valeurs reçues par proximity
-//    	test_value[0] = get_prox(IR0);
-//    	test_value[1] = get_calibrated_prox(IR0);
-//    	chprintf((BaseSequentialStream *)&SD3, "prox = %d \n", test_value[0]);
-//    	chprintf((BaseSequentialStream *)&SD3, "calibrated_prox = %d \n", test_value[1]);
-
+//    	//test des valeurs reçues par proximity
+//    	for(i = 0; i < PROXIMITY_NB_CHANNELS; i++)
+//    	{
+//			test_value[0] = get_prox(i);
+//			test_value[1] = get_calibrated_prox(i);
+//			chprintf((BaseSequentialStream *)&SD3, "IR%d \n", i+1);
+//			chprintf((BaseSequentialStream *)&SD3, "prox = %d \n", test_value[0]);
+//			chprintf((BaseSequentialStream *)&SD3, "calibrated_prox = %d \n", test_value[1]);
+//    	}//conclusion distance[mm] = -0.0625*prox + 54 = -x/16 +54 ??
+//    	//test de calculs de distances
+//    	for(i = 0; i < PROXIMITY_NB_CHANNELS; i++)
+//    	{
+//    		test_value[0] = get_prox(i);
+//    		test_value[1] = (-test_value[0])/DIV_FACTOR_DIST + OFFSET_DIST;
+//    		chprintf((BaseSequentialStream *)&SD3, "IR%d \n", i+1);
+//    		chprintf((BaseSequentialStream *)&SD3, "dist[mm] = %d \n", test_value[1]);
+//    	}
     	//waits 1 second
         chThdSleepMilliseconds(1000);
     }
