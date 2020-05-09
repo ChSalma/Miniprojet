d4d06b5b76f58572c7de0fe1a12639bbf8632974
Overt1me <aurelien.goumaz@epfl.ch>
2020-05-09 11:20:58 +0200

    Cleaning 1.2
    regulator.c

--- regulator.c
+++ regulator.c
@@ -19,50 +19,14 @@
 //on définit un offset
 #define OFFSET 20
 
+enum{DIFFERENCE, FOLLOW_WALL};
+
 static float last_difference = 0;
 
-void regulator_difference(int front_right_45deg_value, int front_left_45deg_value, int front_right_value, int front_left_value)
+//Fonction interne
+void regulator_pd(int16_t difference, uint8_t regulation_type)
 {
-    /*Cette fonction est appelée uniquement quand le robot est entre deux murs.
-     * On utilise les capteurs avant 45deg autant que possible car ils permettent une meilleure anticipation de
-     * l'environnment. Par contre, il est imperatif de ne plus les utiliser lorsqu'ils ne captent plus les murs
-     * entourant le robot car le robot risquerait de dévier de sa trajectoire. C'est pourquoi, on repasse à
-     * une régulation avec les capteurs latéraux dans ce cas là.*/
-
-    int16_t right_speed, left_speed;
-    int16_t difference, derivate;
-	if ((front_right_45deg_value>THRESHOLD_45_DEG)&&(front_left_45deg_value>THRESHOLD_45_DEG))
-	{
-		difference = front_right_45deg_value-front_left_45deg_value;
-	}
-	else
-		difference = front_right_value-front_left_value-OFFSET;
-	/*Pour éviter des corrections angulaires trop fortes, qui pourrait faire dévier le robot
-	 * de sa trajectoire principale, on définit un maximum.*/
-	if(difference > MAX_DIFF)
-		difference = MAX_DIFF;
-	if(difference < -MAX_DIFF)
-		difference = -MAX_DIFF;
-
-	derivate = difference-last_difference;
-
-    //REGULATION PD//
-	right_speed = get_right_speed()+ KP*difference + KD*derivate;
-	left_speed = get_left_speed()-KP*difference-KD*derivate;
-
-	set_speed(right_speed, left_speed);
-	last_difference = difference;
-}
-
-void regulator_follow_wall(int reference_value, int current_value, int sensor_id)
-{
-    int16_t right_speed, left_speed;
-    int16_t difference, derivate;
-
-    difference=reference_value-current_value;
-
-	if (sensor_id==RIGHT_SENS)
-		difference = -difference+OFFSET;
+	int16_t right_speed, left_speed, derivate;
 
 	if(difference > MAX_DIFF)
 		difference = MAX_DIFF;
@@ -72,9 +36,51 @@ void regulator_follow_wall(int reference_value, int current_value, int sensor_id
 	derivate = difference-last_difference;
 
 	//REGULATION PD//
-	right_speed = get_right_speed() + KP_FW*difference + KD_FW*derivate;
-	left_speed = get_left_speed()-KP_FW*difference - KD_FW*derivate;
+	if (regulation_type==DIFFERENCE)
+	{
+		right_speed = get_right_speed() + KP*difference + KD*derivate; 	//Cette méthode consomme moins de mémoire que
+		left_speed = get_left_speed() - KP*difference - KD*derivate;	//de créer deux variables float que l'on passe
+	}																	//comme arguments (p_coeff, d_coeff) pour les
+	else																//bons KP et KD
+	{
+		right_speed = get_right_speed() + KP_FW*difference + KD_FW*derivate;
+		left_speed = get_left_speed() - KP_FW*difference - KD_FW*derivate;
+	}
 
 	set_speed(right_speed, left_speed);
 	last_difference = difference;
+}
+
+//Fonctions publiques
+void regulator_difference(int front_right_45deg_value, int front_left_45deg_value, int front_right_value, int front_left_value)
+{
+    /*Cette fonction est appelée uniquement quand le robot est entre deux murs.
+     * On utilise les capteurs avant 45deg autant que possible car ils permettent une meilleure anticipation de
+     * l'environnment. Par contre, il est imperatif de ne plus les utiliser lorsqu'ils ne captent plus les murs
+     * entourant le robot car le robot risquerait de dévier de sa trajectoire. C'est pourquoi, on repasse à
+     * une régulation avec les capteurs latéraux dans ce cas là.*/
+
+    int16_t difference;
+
+    if ((front_right_45deg_value>THRESHOLD_45_DEG)&&(front_left_45deg_value>THRESHOLD_45_DEG))
+	{
+		difference = front_right_45deg_value-front_left_45deg_value;
+	}
+	else
+		difference = front_right_value-front_left_value-OFFSET;
+	/*Pour éviter des corrections angulaires trop fortes, qui pourrait faire dévier le robot
+	 * de sa trajectoire principale, on définit un maximum.*/
+    regulator_pd(difference, DIFFERENCE);
+}
+
+void regulator_follow_wall(int reference_value, int current_value, int sensor_id)
+{
+    int16_t difference;
+
+    difference=reference_value-current_value;
+
+	if (sensor_id==RIGHT_SENS)
+		difference = -difference+OFFSET;
+
+	regulator_pd(difference, FOLLOW_WALL);
 }
